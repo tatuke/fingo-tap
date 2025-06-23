41aed7a57b068c19c3c13a0b07cf05c5fee889f4
tatuke <screamingoutput@163.com>
2025-06-24 01:58:11 +0800

    fix llm_agent illegal argument

--- src/open_codex/interfaces/llm_agent.py
+++ src/open_codex/interfaces/llm_agent.py
@@ -2,6 +2,6 @@ from abc import ABC, abstractmethod
 
 class LLMAgent(ABC):
     @abstractmethod
-    def one_shot_mode(self, user_input: str, system_info: str) -> str:
+    def one_shot_mode(self, user_input: str) -> str:
         pass
     
